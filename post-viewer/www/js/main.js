9c7c59551d95bf30526eb031fb68e5b18529c1fc
ahmaddo <a.ahmad@outlook.de>
2017-10-31 02:50:23 +0100

    view app name from config file

--- post-viewer/www/js/main.js
+++ post-viewer/www/js/main.js
@@ -1,18 +1,25 @@
 var targetUrl = null;
 var apiUrl = null;
+var appName = null;
 
 $.get('config/config.json').done(function (config) {
     targetUrl = config.api_post_url + config.number_of_posts;
     apiUrl = config.api_url;
+    appName = config.name;
     startApp()
 });
 
 function startApp() {
     $.get(targetUrl).done(
         function (response) {
+            setAppName();
             responsePostActions(response);
         }
     );
+}
+
+function setAppName() {
+    $('.toolbar__center').text(appName);
 }
 
 function responsePostActions(response) {
@@ -57,13 +64,13 @@ function openPost(postLink, featuredImgSrc) {
 
 function openPostPage(post, featuredImgSrc) {
     document.querySelector('#navigator').pushPage('post.html').then(function () {
-        debugger;
         featuredImgSrc = featuredImgSrc.substr(0, featuredImgSrc.length -11) + '279x220.jpg';
         $("#post-ajax-loader").hide();
 
         $("#post-page .content")
             .append('<div class="featured-image-container"><img class="post-featured-image" src="' + featuredImgSrc + '"></img></div>')
             .append('<h3>' + post.title.rendered + '</h3>')
-            .append(post.content.rendered);
+            .append(post.content.rendered)
+            .append('<a href="' + post.link + '">أفتح الموقع</a>');
     })
 }
